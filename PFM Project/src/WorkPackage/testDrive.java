5cee5d6a57dc3c0ef04fc0adb2c46dcf88bfbefb
Lufeng-C <>
2021-03-06 22:01:25 +0800

    Coded part of logIn Class

--- PFM Project/src/WorkPackage/testDrive.java
+++ PFM Project/src/WorkPackage/testDrive.java
@@ -1,10 +1,17 @@
 package WorkPackage;
 
 public class testDrive {
-
+	
+	/*
+	 * The main method is located here for testing
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+				
+		logIn.register();
+		
+		
 	}
 
 }
