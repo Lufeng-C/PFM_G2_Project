324ff500ad00b1277475db8d513467a8cbe1c4f5
LarsvanKranenburg <80324933+LarsvanKranenburg@users.noreply.github.com>
2021-03-09 15:20:13 +0100

    Update unregisteredUser.java

--- PFM Project/src/WorkPackage/unregisteredUser.java
+++ PFM Project/src/WorkPackage/unregisteredUser.java
@@ -6,11 +6,34 @@ public class unregisteredUser {
 
 	static Scanner userInputInt = new Scanner(System.in); 
 	static Scanner userInputString = new Scanner(System.in); 	
+	static Scanner userInputDouble = new Scanner(System.in); 
 	
-	int guestID = Math ;
+	int guestID = (Math.random()*10000) + 1;
 	
 	
+	//Call Registration method (logIn)
 	//Admin has predefined login and username (un: admin, pw: admin123), while loop if un == admin && pw == admin123, bring to admin interface
+		
+		
+	
+	//FilterOptions write out all filters and get input (8 filter options) 
+			//Do you want another, click "Yes" and loop back to get another option
+			//Do you want to continue?
+	
+	
+	//viewRecommendation method 
+	//arraylist and compare with the file of the car. 
+	//You need ReadCarFile to compare filter arrays to existing cars arrays
+	//if (filters[i] = car[i]) for ( i = 0, l=filters.length; i < l; i++) if (filters[i] = car[i]) OR filters[i].equals(car[i]) 
+	//Calculate number of index array1 which is equal to index of array2, then get percentage
+	
+	//Call login method (logIn)
+	
+	//Call register method (logIn)
+	
+	
+	//
+	
 	
 	
 }
