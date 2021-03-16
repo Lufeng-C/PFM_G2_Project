85c410d2592b84746b0c7283d15c345a65374bc9
Lufeng-C <>
2021-03-16 23:53:46 +0800

    Update testDrive.java

--- PFM Project/src/WorkPackage/testDrive.java
+++ PFM Project/src/WorkPackage/testDrive.java
@@ -13,9 +13,16 @@ public class testDrive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		registeredUser.deleteAccount(null);
-		
+
+		ArrayList<registeredUser> userList = new ArrayList<registeredUser>();
+
+		userList = registeredUser.createObjects(); // creates the user objects from 
+		// permRegistration.txt and store them in userList
+
+		registeredUser delete_me = userList.get(0); //get a user object from userList
+
+		registeredUser.deleteAccount(delete_me); //choose this user as input
+
 
 		System.out.println(registeredUser.logIn(userList));
 
@@ -31,7 +38,7 @@ public class testDrive {
 		int Choice = userInputInt.nextInt();
 		switch (Choice) {
 		case 1:
-			registeredUser.logIn(ArrayList<registeredUser> userList);
+			registeredUser.logIn(userList);
 			break;
 		case 2:
 			registeredUser.register();
@@ -39,28 +46,27 @@ public class testDrive {
 		case 3:
 			unregisteredUser.unregisterUserMethod();
 			break;
-
-			ArrayList <registeredUser> userList = registeredUser.createObjects();
-
-			for (int i = 0; i < 5; i++) {
-				System.out.println(userList.get(i).emailAddress);
-			}
-
-			/*
-			 * SO= upon entering the system, in the main class the user gets offered three options:
-			 * 1. login -> enters the login method. So if login == successful -> we store a value = true, which gives you access to addfavourites after recommendation.
-			 * 2. register -> enter the register method
-			 * 3. continue as guest -> move straight to recommendation. 
-			 * 
-			 * Recommendation System starts here
-			 * 1. ...
-			 * ..
-			 * 3. if logInStatus == true, you can access add favourite function
-			 */
-
-
 		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(userList.get(i).getEmailAddress());
+		}
+
+		/*
+		 * SO= upon entering the system, in the main class the user gets offered three options:
+		 * 1. login -> enters the login method. So if login == successful -> we store a value = true, which gives you access to addfavourites after recommendation.
+		 * 2. register -> enter the register method
+		 * 3. continue as guest -> move straight to recommendation. 
+		 * 
+		 * Recommendation System starts here
+		 * 1. ...
+		 * ..
+		 * 3. if logInStatus == true, you can access add favourite function
+		 */
+
+
 	}
+}
 }
 
 
