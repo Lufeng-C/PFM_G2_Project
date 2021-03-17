f25cc6485cffa43ad1002acb0712dad12dcbb748
abdannor <80044858+abdannor@users.noreply.github.com>
2021-03-17 20:30:54 +0100

    Update unregisteredUser.java

--- PFM Project/src/WorkPackage/unregisteredUser.java
+++ PFM Project/src/WorkPackage/unregisteredUser.java
@@ -339,7 +339,7 @@ public class unregisteredUser {
 					this.carArrayList.get(i).getSize().equalsIgnoreCase(carSize)
 					){
 
-				System.out.println("Congratulations! Found a Matching. Recommended Car for you: " 
+				System.out.println("Congratulations! Found a match. Recommended car for you: " 
 						+ "Car name: " + this.carArrayList.get(i).getCarName() + "\t" +
 						"Car price: " + this.carArrayList.get(i).getBasePrice());
 				foundCar = this.carArrayList.get(i);
@@ -370,7 +370,7 @@ public class unregisteredUser {
 					this.carArrayList.get(i).getSize().equalsIgnoreCase(carSize)
 					) {
 
-				System.out.println("Sorry, No matching Found! We only found a car outside your price range: "
+				System.out.println("Sorry, No match Found! We only found a car outside your price range: "
 						+ "Car name: " + this.carArrayList.get(i).getCarName() + "\t" 
 						+ "Car price: " + this.carArrayList.get(i).getBasePrice());
 				foundCar = this.carArrayList.get(i);
