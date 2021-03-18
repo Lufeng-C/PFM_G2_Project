e4353c5770a0c4c2b5340e9312e463267057e2b6
Lufeng-C <>
2021-03-18 20:24:46 +0800

    Further coded main method

--- PFM Project/src/WorkPackage/testDrive.java
+++ PFM Project/src/WorkPackage/testDrive.java
@@ -15,29 +15,13 @@ public class testDrive {
 	 */
 
 	public static void main(String[] args) {
-
+	
+		// creates the user objects from permRegistration.txt and store them in userList
 		ArrayList<registeredUser> userList = new ArrayList<registeredUser>();
-
-		userList = registeredUser.createObjects(); // creates the user objects from 
-		// permRegistration.txt and store them in userList
-
-
-
+		userList = registeredUser.createObjects(); 
+		
+		// go to the first menu (and subsequent ones from there)
 		unregisteredUser.beginMenu(userList);
-
-
-		/*
-		 * SO= upon entering the system, in the main class the user gets offered three options:
-		 * 1. login -> enters the login method. So if login == successful -> we store a value = true, which gives you access to addfavourites after recommendation.
-		 * 2. register -> enter the register method
-		 * 3. continue as guest -> move straight to recommendation. 
-		 * 
-		 * Recommendation System starts here
-		 * 1. ...
-		 * ..
-		 * 3. if logInStatus == true, you can access add favourite function
-		 */
-
 
 	}
 }
