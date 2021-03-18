4e73b4fb318b7624f8f6165126b993f1abe55bf3
abdannor <80044858+abdannor@users.noreply.github.com>
2021-03-18 23:44:24 +0100

    Update testDrive.java

--- PFM Project/src/WorkPackage/testDrive.java
+++ PFM Project/src/WorkPackage/testDrive.java
@@ -10,17 +10,19 @@ public class testDrive {
 	static Scanner userInputInt = new Scanner(System.in); 
 	static Scanner userInputString = new Scanner(System.in); 
 	static Scanner userInputDouble = new Scanner(System.in);
+	private ArrayList<car> carArrayList; //car arraylist
 	/*
 	 * The main method is located here for testing
 	 */
 
 	public static void main(String[] args) {
-	
+		
 		// creates the user objects from permRegistration.txt and store them in userList
 		ArrayList<registeredUser> userList = new ArrayList<registeredUser>();
 		userList = registeredUser.createObjects(); 
 		
 		// go to the first menu (and subsequent ones from there)
+		unregisteredUser.main();
 		unregisteredUser.beginMenu(userList);
 
 	}
