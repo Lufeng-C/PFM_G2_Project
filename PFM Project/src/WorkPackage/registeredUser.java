dba95bf649d9fc5e3a90f869835a1aacfc876bc2
abdannor <80044858+abdannor@users.noreply.github.com>
2021-03-19 21:04:21 +0100

    Update registeredUser.java

--- PFM Project/src/WorkPackage/registeredUser.java
+++ PFM Project/src/WorkPackage/registeredUser.java
@@ -398,53 +398,19 @@ public class registeredUser extends unregisteredUser { // created inheritance
 			int indx = carIDList.indexOf(Integer.parseInt(favCarID));
 			car favCar = carArrayList.get(indx);
 			
-			System.out.print("\nYour favorite car is:\n ****" + favCar.getCarName() + "****\n");
+			System.out.print("\nYour favorite car is:\n****" + favCar.getCarName() + "****\n");
 			System.out.printf("***Car details*** \n"
-					+"Car name: %s\n"
 					+ "Car type: %s\n"
-					+ "Base price: %s\n"
+					+ "Base price: €%s\n"
 					+ "Size: %s\n"
 					+ "Sport: %s\n"
 					+ "Fuel type: %s\n",
-					favCar.getCarName(), favCar.getCarType(), favCar.getBasePrice(), 
+					favCar.getCarType(), favCar.getBasePrice(), 
 					favCar.getSize(), favCar.isSport(), favCar.getFuelType());
 			
 			System.out.println("\n");
 			this.userInterface();
 			
-		}
-			/*+ ". Would you like to view more details [1] YES  [2] NO? ");
-			try {
-				int choiceDetails = userInputInt.nextInt();
-				switch (choiceDetails) {
-				case 1:
-					System.out.printf("Car name: %s\n"
-							+ "Car type: %s\n"
-							+ "Base price: %s\n"
-							+ "Size: %s\n"
-							+ "Sport: %s\n"
-							+ "Fuel type: %s\n",
-							favCar.getCarName(), favCar.getCarType(), favCar.getBasePrice(), 
-							favCar.getSize(), favCar.isSport(), favCar.getFuelType());
-					System.out.println( "\nThanks for viewing your favorite car!"
-							+ "\nYou are now being redirected to the previous menu." 
-							+ "\n*******************************\n\n");
-					this.userInterface();
-				case 2:
-					System.out.println( "\nThanks for viewing your favorite car!"
-							+ "\nYou are now being redirected to the previous menu." 
-							+ "\n*******************************\n\n");
-					this.userInterface();
-			
-				default: 
-					System.out.println("\nInvalid Choice!");
-					this.viewFavorite(carArrayList);
-					break;
-				}//end switch
-			} catch (Exception ex) {
-				System.out.println("Something went wrong for I/O!"
-						+ "Catch source: viewFavorite, registeredUser:462");
-			} //end catch & try
 		}//end if*/
 		else {
 			System.out.println("\nYou haven't got any favorite car yet."
