f4b7594e96459c3c4e850d560bba6019dd4e70e5
abdannor <80044858+abdannor@users.noreply.github.com>
2021-03-19 15:09:24 +0100

    Update unregisteredUser.java

--- PFM Project/src/WorkPackage/unregisteredUser.java
+++ PFM Project/src/WorkPackage/unregisteredUser.java
@@ -68,7 +68,7 @@ public class unregisteredUser {
 			System.out.println("Welcome guest, please register for more facilities.\n" +
 					"for now, you can create one recommendation. Thank You!");
 			unregisteredUserMethod();
-			System.out.println("You created one recommendation. Please register for more facilities. You exited the system now");
+			System.out.println("You created one recommendation. Please register for more facilities. You exited the system now.");
 			break;
 		case 4:
 			System.out.println("\n\nThanks for using our system."
